a7d16f3bbfed410a87145bda2c58439b13c9cece
Sumatokhin Vladislav <sumatokhin.v.a@gmail.com>
2023-08-19 18:59:44 +0300

    Refactor experience section: - Create responsive ExperienceCard component. - Enhance ExperienceAccordion with default expand option. - Remove redundant comments and elements.

--- src/components/experienceCard/ExperienceCard.js
+++ src/components/experienceCard/ExperienceCard.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import './ExperienceCard.css';
+import React, { Component } from "react";
+import "./ExperienceCard.css";
 import { Fade } from "react-reveal";
 
 class ExperienceCard extends Component {
@@ -8,58 +8,41 @@ class ExperienceCard extends Component {
     const index = this.props.index;
     const totalCards = this.props.totalCards;
     const theme = this.props.theme;
+    const orientation = this.props.orientation
+      ? this.props.orientation
+      : "horizontal";
+
     return (
-      <div className="experience-list-item" style={{ marginTop: (index === 0 ? 30 : 50) }}>
-        <Fade left duration={2000} distance="40px">
-          <div className="experience-card-logo-div">
+      <Fade right duration={2000} distance="40px">
+        <div
+          className={`experience-card ${orientation}`}
+          style={{ marginTop: index === 0 ? 20 : 10 }}
+        >
+          <div className="position-description">
+            <div className={`position-box`}>
+              <div className={`position-name`}>{experience["title"]}</div>
+            </div>
+            <div className="location-duration">
+              <div className="text-wrapper">{experience["location"]}</div>
+              <div className="text-wrapper">{experience["duration"]}</div>
+            </div>
             <img
-              className="experience-card-logo"
+              className="company-logo"
               src={require(`../../assests/images/${experience["logo_path"]}`)}
               alt=""
             />
+            <div className="company-name">{experience["company"]}</div>
+            <p className="company-description">{experience["description"]}</p>
           </div>
-        </Fade>
-        <div className="experience-card-stepper">
-          <div style={{ width: 20, height: 20, backgroundColor: `${theme.headerColor}`, borderRadius: 50, zIndex: 100 }} />
-          {
-            index !== (totalCards - 1) &&
-            <div style={{ height: 190, width: 2, backgroundColor: `${theme.headerColor}`, position: 'absolute', marginTop: 20 }} />
-          }
+          <div className="responsibilities">
+            <ul>
+              {experience["responsibilities"].map((responsibility, index) => {
+                return <li className="responsibility">{responsibility}</li>;
+              })}
+            </ul>
+          </div>
         </div>
-        <Fade right duration={2000} distance="40px">
-          <div style={{ display: 'flex', flexDirection: 'row' }}>
-            <div className="arrow-left" style={{ borderRight: `10px solid ${theme.body}` }}></div>
-            <div className="experience-card" style={{ background: `${theme.body}` }}>
-              <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
-                <div>
-                  <h3 className="experience-card-title" style={{ color: theme.text }}>{experience["title"]}</h3>
-                  <p className="experience-card-company" style={{ color: theme.text }}>
-                    <a href={experience["company_url"]} target="_blank" rel="noopener noreferrer">
-                      {experience["company"]}
-                    </a>
-                  </p>
-                </div>
-                <div>
-                  <div className="experience-card-heading-right">
-                    <p className="experience-card-duration" style={{ color: theme.secondaryText }}>{experience["duration"]}</p>
-                    <p className="experience-card-location" style={{ color: theme.secondaryText }}>{experience["location"]}</p>
-                  </div>
-
-                </div>
-              </div>
-              <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: 20 }}>
-                <div
-                  className="repo-description"
-                />
-                {experience["description"]}
-              </div>
-
-            </div>
-
-          </div>
-        </Fade>
-      </div>
-
+      </Fade>
     );
   }
 }
--- src/containers/experienceAccordion/ExperienceAccordion.js
+++ src/containers/experienceAccordion/ExperienceAccordion.js
@@ -1,52 +1,71 @@
 import React, { Component } from "react";
 import ExperienceCard from "../../components/experienceCard/ExperienceCard.js";
 import "./ExperienceAccordion.css";
-import { Accordion, Panel } from "baseui/accordion";
+import { StatelessAccordion, Panel } from "baseui/accordion";
+import { Fade } from "react-reveal";
 
 class ExperienceAccordion extends Component {
+  state = {
+    expanded: ["Work"], // Start with the first section expanded
+  };
+
   render() {
     const theme = this.props.theme;
     return (
-      <div className="experience-accord">
-        <Accordion>
-          {this.props.sections.map((section) => {
-            return (
-              <Panel
-                className="accord-panel"
-                title={section["title"]}
-                key={section["title"]}
-                overrides={{
-                  Header: {
-                    style: () => ({
-                      backgroundColor: `${theme.body}`,
-                      border: `1px solid`,
-                      borderRadius: `5px`,
-                      borderColor: `${theme.headerColor}`,
-                      marginBottom: `3px`,
-                      fontFamily: "Google Sans Regular",
-                      color: `${theme.text}`,
-                      ":hover": {
-                        color: `${theme.secondaryText}`,
-                      },
-                    }),
+      <Fade right duration={2000} distance="40px">
+        <div className="experience-accord">
+          <StatelessAccordion
+            expanded={this.state.expanded}
+            onChange={({ expanded }) => this.setState({ expanded })}
+            overrides={{
+              Header: {
+                style: () => ({
+                  backgroundColor: `${theme.body}`,
+                  border: `1px solid`,
+                  borderRadius: `5px`,
+                  borderColor: `${theme.headerColor}`,
+                  marginBottom: `3px`,
+                  fontFamily: "Google Sans Regular",
+                  color: `${theme.text}`,
+                  ":hover": {
+                    color: `${theme.secondaryText}`,
                   },
-                  Content: {
-                    style: () => ({
-                      backgroundColor: `${theme.body}`,
-                    }),
-                  },
-                }}
-              >
-{section["experiences"].map((experience,index) => {
-                  return (
-                    <ExperienceCard index={index} totalCards={section["experiences"].length} experience={experience} theme={theme} />
-                  );
-                })}
-              </Panel>
-            );
-          })}
-        </Accordion>
-      </div>
+                }),
+              },
+              Content: {
+                style: () => ({
+                  backgroundColor: `${theme.body}`,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  flexDirection: "column",
+                }),
+              },
+            }}
+          >
+            {this.props.sections.map((section) => {
+              return (
+                <Panel
+                  className="accord-panel"
+                  title={section["title"]}
+                  key={section["title"]}
+                >
+                  {section["experiences"].map((experience, index) => {
+                    return (
+                      <ExperienceCard
+                        index={index}
+                        totalCards={section["experiences"].length}
+                        experience={experience}
+                        theme={theme}
+                      />
+                    );
+                  })}
+                </Panel>
+              );
+            })}
+          </StatelessAccordion>
+        </div>
+      </Fade>
     );
   }
 }
