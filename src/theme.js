f6c12e99b240894a7ec3db58bee94f964075b79b
Sumatokhin Vladislav <sumatokhin.v.a@gmail.com>
2023-08-19 21:12:38 +0300

    Added new theme

--- src/theme.js
+++ src/theme.js
@@ -195,4 +195,19 @@ export const materialTealTheme = {
   splashBg: "#05505E",
 };
 
-export const chosenTheme = blueTheme;
+export const yellowDarkTheme = {
+  body: "#43454b",
+  text: "#ffc13b",
+  expTxtColor: "#000000",
+  highlight: "#43454b",
+  dark: "#2c2e35",
+  secondaryText: "#f5f6f6",
+  imageHighlight: "#fdca5c",
+  compImgHighlight: "#fdca5c",
+  cardBackground: "#f5f6f6",
+  jacketColor: "#ffc13b",
+  headerColor: "#43454b",
+  splashBg: "#43454b",
+};
+
+export const chosenTheme = yellowDarkTheme;
